789741f2a9ecc2b2496738f632a6cff6f5a0b095
Tero Hasu <tero.hasu@hut.fi>
2012-12-12 03:18:00 +0100

    very basic syntax to form conversion

--- main.rkt
+++ main.rkt
@@ -9,17 +9,35 @@ program.
 
 |#
 
-(require "util.rkt")
-(require (for-syntax racket "util.rkt"))
+(require "form.rkt" "util.rkt")
+(require (for-syntax racket "form.rkt" "util.rkt"))
 
 (provide (rename-out (my-module-begin #%module-begin)))
+
+(define-for-syntax (syntax->form stx)
+  (let ((e (syntax-e stx)))
+    (cond
+     ((symbol? e)
+      (form e))
+     ((null? e)
+      (form e))
+     ((pair? e)
+      (syntax-list->form e)) ;; xxx may not be a list
+     (else
+      (form e)))))
+
+(define-for-syntax (syntax-list->form lst)
+  (form (map syntax->form lst)))
+
+(define-for-syntax (list-syntax->form stx)
+  (let ((lst (syntax->list stx)))
+    (syntax-list->form lst)))
 
 (define-syntax (my-module-begin stx)
   (syntax-case stx ()
       ((_ body ...)
        (begin
-         (for-each
-          pretty-print
-          (syntax->datum #'(body ...)))
-         #`(#%module-begin
-            )))))
+         (let ((f (list-syntax->form #'(body ...))))
+           (pretty-print f)
+           (pretty-print (form->datum f)))
+         #`(#%module-begin)))))
