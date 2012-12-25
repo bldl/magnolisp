5c56b92bc1c8d055c114492f5148a507dc490448
Tero Hasu <tero.hasu@hut.fi>
2012-12-25 13:03:10 +0100

    identifier=?

---
+++ env.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+#|
+
+Environment. Based directly on Rackets functional-update hash with eq?
+comparisons, but we nonetheless treat it as an abstract data type.
+
+|#
+
+(require "util.rkt")
+
+(provide (rename-out
+          (hasheq env-new)
+          (hash? env?)
+          (hash-set env-set)
+          (hash-ref env-ref)
+          (hash-has-key? env-has?)
+          (hash-remove env-remove)
+          (hash-keys env-names)))
+
+(define* (env-get env name)
+  (hash-ref env name
+            (thunk (error 'env-get "undefined name: ~a" name))))
+
+;; 'id' is a 'gensym'ed unique name for the named thing, which is (1)
+;; a special form, (2) a macro, or (3) a variable. Top-level names
+;; must be unique, and have their name as their 'id'.
+(struct* binding (id) #:transparent)
+(struct* special-b binding (eval compile) #:transparent)
+(struct* macro-b binding (expand mode) #:transparent)
+(struct* var-b binding (decl) #:transparent)
--- macro.rkt
+++ macro.rkt
@@ -5,19 +5,20 @@
 Defines a data type for syntactic closures. A macro system with
 various APIs can be built on top.
 
-The API of this module should be exposed to the Racket namespace in
-effect for macro expansion. This should probably be done
-using "standard" names, excluding any clashing names that may already
-be defined for Racket. This makes it easier to port over existing
-macro constructs defined in Scheme.
+The API of this module should be included in the Racket namespace that
+is in effect for macro expansion. This should probably be done
+using "standard" names, by not including any clashing names that are
+already defined in the "racket" language. Using typical names makes it
+easier to port over existing macro constructs defined in Scheme.
 
 |#
 
-(require "form.rkt" "util.rkt")
+(require "env.rkt" "form.rkt" "util.rkt")
 
 (struct synclo (env names exp) #:transparent)
 
-(provide (rename-out (synclo make-syntactic-closure)))
+(provide (rename-out
+          (synclo make-syntactic-closure)))
 
 ;; Does not strip from annotations.
 (define* (strip-syntactic-closures e)
@@ -30,3 +31,25 @@ macro constructs defined in Scheme.
     (map strip-syntactic-closures e))
    (else
     e)))
+
+;; If a symbol (possibly a 'form') or syntactic closure enclosed
+;; symbol. Note that 'racket/base' includes 'identifier?'.
+(define* (my-identifier? x)
+  (or (symbol? x)
+      (and (form? x) (my-identifier? (form-datum x)))
+      (and (synclo? x) (my-identifier? (synclo-exp x)))))
+
+(define* (identifier->symbol x)
+  (cond
+   ((symbol? x) x)
+   ((form? x) (identifier->symbol (form-datum x)))
+   ((synclo? x) (identifier->symbol (synclo-exp x)))
+   (else (error 'identifier->symbol "not an identifier: ~s" x))))
+
+;; We assign unique identifiers to all declared names, which makes
+;; identifier comparisons straightforward.
+(define* (identifier=? env-1 id-1 env-2 id-2)
+  (let ((item-1 (env-get env-1 id-1))
+	(item-2 (env-get env-2 id-2)))
+    (eq? (binding-id item-1)
+         (binding-id item-2))))
