e1e536b002b0b04f50402a2e3e783a0ddc5771f4
Tero Hasu <tero.hasu@hut.fi>
2013-03-13 22:15:11 +0100

    %compilation-unit indirection

--- compiler.rkt
+++ compiler.rkt
@@ -22,6 +22,9 @@ language.
 (require syntax/modcode syntax/moddep syntax/modresolve) 
 (require syntax/toplevel)
 
+;; Need to import this so that gets correct binding.
+(require (only-in "runtime-compiler.rkt" %compilation-unit))
+
 ;; cannot use unless transforming
 ;(expand-import #'"util.rkt")
 
@@ -37,28 +40,10 @@ language.
       (let ((core-stx
              (expand-syntax-top-level-with-compile-time-evals
               (namespace-syntax-introduce
-               #`(begin #,@stx-lst)))))
-        (syntax->datum core-stx)))))
+               #`(%compilation-unit #,@stx-lst)))))
+        (pretty-println (syntax->datum core-stx))))))
 
 (define* (compile-module mn)
   (compile-file (resolve-module-path mn #f)))
 
 (compile-module "try-program-4.rkt")
-
-#;
-(define-syntax (my-module-begin stx)
-  (syntax-case stx ()
-    ((_ body ...)
-     (let ((code
-            (let ((ns (make-base-empty-namespace)))
-              (parameterize ((current-namespace ns))
-                (namespace-require '(for-syntax racket))
-                (namespace-require 'racket/base) ;; xxx we do not really want to do this, but may need to have some of it for macro expansion to work -- but does not appear to make it happen -- we only appear to be getting partial results, though -- forms such as 'unless' do expand, but all macro applications
-                (namespace-require '"runtime-compiler.rkt")
-                (expand-syntax-top-level-with-compile-time-evals
-                 ;; namespace-syntax-introduce seems to make no difference -- xxx how do we get our macros to expand, the ones given in runtime.rkt
-                 (namespace-syntax-introduce #'(begin body ...))))))
-           (%code (format-id stx "%code")))
-       #`(#%plain-module-begin
-          (define #,%code '#,code) ;;xxx store expanded form -- note that expansion requires "runtime" library (for anno macros etc.) -- we must also be able to access some configuration information for compiler options (do not know how yet)
-          (provide #,%code))))))
--- runtime-compiler.rkt
+++ runtime-compiler.rkt
@@ -1,22 +1,32 @@
-#lang racket/base
+#lang racket
 
 #|
 
 |#
 
-(require "util.rkt")
-(require "runtime-common.rkt")
+(require "util.rkt" (for-syntax "util.rkt"))
+(require (for-syntax syntax/context))
 
+(require "runtime-common.rkt")
 (provide (all-from-out "runtime-common.rkt"))
 
 ;; Merely by exporting these almost anything may appear in a top-level
-;; program. Such an expansion is not very useful, however.
+;; program. Such an expansion is not very useful, however. We can
+;; instead just list our core language in a stoplist.
 (provide #%app #%top)
+
+(define-syntax* (%compilation-unit stx)
+  (let ((stx-lst (cdr (syntax->list stx)))
+        (def-ctx (syntax-local-make-definition-context))
+        (ctx (generate-expand-context))
+        )
+    #`(begin #,@stx-lst)
+    ))
+    ;;local-expand  
 
 ;;; 
 ;;; declarations
 ;;; 
-
 
 ;;; 
 ;;; statements
