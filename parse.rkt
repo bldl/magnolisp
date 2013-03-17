59c2e5c0783639285a6ce431d892de986df9dec3
Tero Hasu <tero.hasu@hut.fi>
2013-03-17 03:34:12 +0100

    match k-app, k-lambda

--- parse.rkt
+++ parse.rkt
@@ -18,39 +18,40 @@ identifiers.
 ;; Drops macro definitions, does alpha conversion, and turns the input
 ;; syntax object into an AST.
 (define* (parse mod-stx)
-  (define (p stx)
-    (syntax-case stx (#%plain-module-begin
-                      define-values
-                      module quote rt.%core)
+  (define (prs stx)
+    (syntax-case stx (k-app k-lambda
+                      #%plain-module-begin
+                      define-values module quote
+                      rt.%core)
 
       ((module n pn (#%plain-module-begin body ...))
-       (new-Module stx (p-lst (syntax->list #'(body ...)))))
+       (new-Module stx (prs-lst (syntax->list #'(body ...)))))
 
-      ((_ rt.%core (quote n) _)
+      ((k-app rt.%core (quote n) _)
        (eq? 'pass (syntax-e #'n))
        (new-Pass stx))
 
-      ((_ rt.%core (quote n) id-stx)
+      ((k-app rt.%core (quote n) id-stx)
        (eq? 'call (syntax-e #'n))
        (new-Call stx (Var-from-stx #'id-stx)))
 
       ((define-values (n) def)
        (let ((def-stx #'def))
-         (syntax-case def-stx (quote rt.%core)
-           ((_ rt.%core (quote t) (_ () body ...))
+         (syntax-case def-stx (k-lambda quote rt.%core)
+           ((k-app rt.%core (quote t) (k-lambda () body ...))
             (eq? 'procedure (syntax-e #'t))
             (new-Define stx (Var-from-stx #'n)
                         'procedure
-                        (p-lst (syntax->list #'(body ...)))))
+                        (prs-lst (syntax->list #'(body ...)))))
            (else
             (error "not core language 'define'" stx)))))
 
       (else (error "not core language" stx))))
 
-  (define (p-lst lst)
-    (filter Ast? (map p lst)))
+  (define (prs-lst lst)
+    (filter Ast? (map prs lst)))
 
-  (p mod-stx))
+  (prs mod-stx))
 
 (define* (print-stx-with-bindings stx)
   (define lst (syntax->list stx))
