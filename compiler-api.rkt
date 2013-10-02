e77f1a4af477260d5f8fc062c861dc9f8baee97c
Tero Hasu <tero@ii.uib.no>
2013-10-02 19:45:49 +0200

    routine for deriving cxx friendly unique module names from module path

--- compiler-api.rkt
+++ compiler-api.rkt
@@ -27,6 +27,115 @@ external dependencies for the program/library, as well as the .cpp and
 (require "util.rkt"
          syntax/id-table syntax/moddep)
 
+;;; 
+;;; canonical module naming
+;;; 
+
+;; We want to pick a name here such that it will also be suitable for
+;; use in C++. For consistency, we use lowercase names, without
+;; underscores.
+
+;; Any module paths are resolved relative to this path. May be #f, in
+;; which case the current directory is used.
+(define* mp-root-path (make-parameter #f))
+
+(define (next-gensym r sym)
+  (define num (hash-ref r sym 0))
+  (define n-sym
+    (if (= num 0)
+        sym
+        (string->symbol
+         (string-append (symbol->string sym)
+                        (number->string num)))))
+  (values (hash-set r sym (+ num 1)) n-sym))
+
+(define (string-keep-basic-chars s)
+  (set! s (regexp-replace #rx"^[^a-zA-Z_]+" s ""))
+  (set! s (regexp-replace* #rx"[^a-zA-Z0-9_]+" s ""))
+  s)
+
+(define (path-basename-only fn)
+  (define-values (p f dir?) (split-path fn))
+  (path-replace-suffix f ""))
+
+(define-syntax-rule (unsupported v ...)
+  (error "unsupported" v ...))
+
+(define (resolved-mp->preferred-name mp)
+  (match mp
+    ((? path?)
+     (string->symbol
+      (string-titlecase
+       (string-keep-basic-chars
+        (path->string
+         (path-basename-only mp))))))
+    ((? symbol?)
+     (string->symbol
+      (string-titlecase
+       (string-keep-basic-chars
+        (symbol->string mp)))))
+    ((list 'submod sub-mp (? symbol? name) ...)
+     ;; Might also be (list 'submod sub-mp (? symbol? name) ...), but
+     ;; sub-mp might not be resolved, and it might already been
+     ;; assigned a name as well.
+     (unsupported "resolved submodule path" mp))))
+
+;; 'h' gives any existing mp -> name mappings. 'r' maps reserved
+;; symbols to the next free number. 'mp' is the module path for which
+;; to get an assigned, unique name. Note that this function cannot
+;; handle module paths such as (quote #%kernel).
+(define (mp->assigned-name h r mp)
+  (let/ec return
+    (define n (hash-ref h mp #f))
+    (when n
+      (return h r n))
+    (define r-mp (resolve-module-path mp (mp-root-path)))
+    (unless (equal? mp r-mp)
+      (let-values (([h r n] (mp->assigned-name h r r-mp)))
+        (return (hash-set h mp n) r n)))
+    (match mp
+      ((or (? path?) (? symbol?))
+       (let*-values (([n] (resolved-mp->preferred-name mp))
+                     ([r n] (next-gensym r n)))
+         (return (hash-set h mp n) r n)))
+      ((list 'submod sub-mp (? symbol? name) ...)
+       (let*-values (([h r sub-n] (mp->assigned-name h r sub-mp))
+                     ([r n] (next-gensym
+                             r
+                             (string->symbol
+                              (string-join
+                               (cons
+                                (symbol->string sub-n)
+                                (map
+                                 (compose string-titlecase
+                                          string-keep-basic-chars
+                                          symbol->string)
+                                 name))
+                               "_")))))
+         (return (hash-set h mp n) r n))))))
+
+#;
+(let ((h #hash())
+      (r #hasheq())
+      (n #f)
+      (mp-lst '(racket/base
+                "compiler-api.rkt"
+                (submod "compiler-api.rkt" foo)
+                (submod "compiler-api.rkt" foo-)
+                (submod "compiler-api.rkt" foo)
+                (submod racket/base foo)
+                (submod "compiler-api.rkt" foo-)
+                (submod "compiler-api.rkt" bar baz)
+                racket/base
+                "compiler-api.rkt")))
+  (for ((mp mp-lst))
+    (set!-values [h r n] (mp->assigned-name h r mp))
+    (writeln (list mp n))))
+   
+;;; 
+;;; other
+;;; 
+
 ;; Compilation state.
 (struct St () #:transparent)
 
@@ -34,7 +143,9 @@ external dependencies for the program/library, as well as the .cpp and
 (define* (new-state)
   (St))
 
-;; Returns an updated compilation state.
+;; Updates compilation state with all the entry points in the
+;; specified module, and all dependencies thereof. Returns an updated
+;; compilation state.
 (define* (compile-module st mp)
   (define m-annos
     (dynamic-require `(submod ,mp definfo) 'm-annos))
@@ -42,11 +153,16 @@ external dependencies for the program/library, as well as the .cpp and
     (dynamic-require `(submod ,mp definfo) 'm-ast))
   st)
 
-;; Returns an updated compilation state.
+;; Compiles the module defined in the specified file. Returns an
+;; updated compilation state.
 (define* (compile-file st fn)
   (define s (if (path? fn) (path->string fn) fn))
   (define mp `(file ,s))
   (compile-module st mp))
+
+;;; 
+;;; code generation
+;;; 
 
 (define* (write-cpp-file st cpp-file)
   (void)) ;;xxx
@@ -56,6 +172,10 @@ external dependencies for the program/library, as well as the .cpp and
 
 (define* (write-mk-file st mk-file)
   (void)) ;;xxx
+
+;;; 
+;;; testing
+;;; 
 
 (module* main #f
   (define st (new-state))
