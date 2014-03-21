fcac4b5928a6d9a7c3fe33ffbe24063d51a8fd31
Tero Hasu <tero@ii.uib.no>
2014-03-21 18:25:35 +0100

    'default' macro test case

---
+++ tests/test-macro-1.rkt
@@ -0,0 +1,26 @@
+#lang magnolisp
+
+(typedef int (#:annos foreign))
+
+(function (equal x y)
+  (#:annos (type (fn int int predicate)) foreign)
+  (equal? x y))
+
+(function (compute x)
+  x)
+
+(define-syntax-rule
+  (default e f d)
+  (do
+    (var ret e)
+    (return (if (equal ret f)
+                d
+                ret))))
+
+(function (f x)
+  (#:annos export)
+  (default (compute x) 0 7))
+
+(default 1 1 7)
+(default 2 1 7)
+
