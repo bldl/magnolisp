d644aa95d0600c01902f99b25ff9b7414ead3877
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-04-09 15:39:20 +0200

    Fix deps.
    See error here: https://plt.eecs.northwestern.edu/pkg-build/server/built/fail/magnolisp.txt

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 (define scribblings '(("manual-src/magnolisp.scrbl" () (experimental))))
 (define racket-launcher-libraries '("compiler-cli.rkt"))
 (define racket-launcher-names '("mglc"))
-(define deps '(("base" #:version "6.3") "data-lib" "scribble-lib"))
+(define deps '(("base" #:version "6.3") "data-lib" "scribble-lib" "unstable-debug-lib"))
 (define build-deps '("at-exp-lib" "racket-doc" "rackunit-lib"))
 (define compile-omit-paths '("dist" "doc" "etc" "failing" "gh-pages" "retired" "tests"))
 (define test-omit-paths '("dist" "doc" "etc" "failing" "gh-pages" "retired"))
